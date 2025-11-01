7ed8a5c1bd9d436396f380363d21e062844beb49
mariam <mariam.leffi4@gmail.com>
2025-11-01 20:13:44 +0100

    Chat Bidirectionnel et Diffusion à Plusieurs Clients

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,44 +1,53 @@
 package clientPackage;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
             DatagramSocket socket = new DatagramSocket();
-            Scanner sc = new Scanner(System.in);
+            InetAddress serveurIP = InetAddress.getByName("localhost");
+            int serveurPort = 1234;
 
-            System.out.println("Client UDP connecté !!");
+            Scanner sc = new Scanner(System.in);
             System.out.print("Entrez votre nom d'utilisateur : ");
             String username = sc.nextLine();
 
-            while (true) {
-                System.out.print("Entrez un message (ou 'exit' pour quitter) : ");
-                String message = sc.nextLine();
+            // Thread pour recevoir les messages
+            Thread receiver = new Thread(() -> {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    try {
+                        DatagramPacket paquet = new DatagramPacket(buffer, buffer.length);
+                        socket.receive(paquet);
+                        String msg = new String(paquet.getData(), 0, paquet.getLength());
+                        System.out.println("\n" + msg);
+                        System.out.print("> ");
+                    } catch (IOException e) {
+                        break;
+                    }
+                }
+            });
+            receiver.start();
 
+            // Thread principal pour envoyer des messages
+            while (true) {
+                System.out.print("> ");
+                String message = sc.nextLine();
                 if (message.equalsIgnoreCase("exit")) {
-                    System.out.println("Fermeture du client");
+                    System.out.println("Déconnexion du client...");
+                    socket.close();
                     break;
                 }
-
-                // Préfixer le message avec le nom d'utilisateur
                 String msgComplet = "[" + username + "] : " + message;
-                byte[] buffer = msgComplet.getBytes();
-
-                // Création du datagramme à envoyer
-                DatagramPacket paquet = new DatagramPacket(buffer,buffer.length,InetAddress.getByName("localhost"),1234);
-                       
-                // Envoi du message au serveur
+                DatagramPacket paquet = new DatagramPacket(msgComplet.getBytes(), msgComplet.length(),
+                        serveurIP, serveurPort);
                 socket.send(paquet);
             }
 
             sc.close();
-            socket.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,33 +1,45 @@
 package serverPackage;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
+import java.net.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            // Liaison du socket serveur au port 1234 avec InetSocketAddress
-            InetSocketAddress adresse = new InetSocketAddress(1234);
-            DatagramSocket socket = new DatagramSocket(adresse);
+            DatagramSocket socket = new DatagramSocket(new InetSocketAddress(1234));
+            System.out.println("=== Serveur UDP lancé sur le port 1234 ===");
 
-            System.out.println("=>Serveur UDP lancé sur le port 1234!!");
+            Set<SocketAddress> clients = new HashSet<>();
+
+            byte[] buffer = new byte[1024];
+
             while (true) {
-                // Préparer le buffer pour recevoir un message
-                byte[] buffer = new byte[1024];
                 DatagramPacket paquetRecu = new DatagramPacket(buffer, buffer.length);
-
-                // Réception d’un message
                 socket.receive(paquetRecu);
 
-                // Extraction du message reçu
-                int taille = paquetRecu.getLength();
-                String msg = new String(paquetRecu.getData(), 0, taille);
+                String msg = new String(paquetRecu.getData(), 0, paquetRecu.getLength());
+                SocketAddress clientAddr = paquetRecu.getSocketAddress();
 
-                // Affichage des infos du client et du message
-                System.out.println("Message reçu de : " + paquetRecu.getAddress().getHostAddress()+ " : " + paquetRecu.getPort());
-                System.out.println(msg);
+                // Ajouter le client si c'est un nouvel expéditeur
+                clients.add(clientAddr);
+
+                System.out.println("Message reçu de " + paquetRecu.getAddress().getHostAddress()
+                                   + ":" + paquetRecu.getPort() + " -> " + msg);
+
+                // Diffuser le message à tous les autres clients
+                for (SocketAddress addr : clients) {
+                    if (!addr.equals(clientAddr)) {
+                        DatagramPacket paquetDiff = new DatagramPacket(
+                                msg.getBytes(),
+                                msg.length(),
+                                ((InetSocketAddress) addr).getAddress(),
+                                ((InetSocketAddress) addr).getPort()
+                        );
+                        socket.send(paquetDiff);
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
